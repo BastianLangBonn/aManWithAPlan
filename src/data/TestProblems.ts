871befa45baa537582e2c8a6f42737d7b7a70f58
BastianLangBonn <bastianlang@t-online.de>
2021-10-17 00:00:29 +0200

    add more acceptance tests

--- src/data/TestProblems.ts
+++ src/data/TestProblems.ts
@@ -10,62 +10,43 @@ export const runToTheHills = [
     "CASTLE 4 0",
     "PRINCESS 2 2"];
 
-    /*
-Run to the hills
-5 5 3
-PRINCESS
-GGGGI
-GGGGG
-GGIGG
-GGGGG
-IGGGG
-HOUSE 0 4
-CASTLE 4 0
-PRINCESS 2 2
-->
-9
+export const hallOfTheMountainKing = [
+    "5 5 4",
+    "TREASURE",
+    "IMMMI",
+    "MMMMM",
+    "GGIMM",
+    "GGGGG",
+    "IGGGG",
+    "HOUSE 0 4",
+    "CASTLE 4 0",
+    "STABLE 2 2",
+    "TREASURE 0 0",
+];
 
-Hall of the mountain king
-5 5 4
-TREASURE
-IMMMI
-MMMMM
-GGIMM
-GGGGG
-IGGGG
-HOUSE 0 4
-CASTLE 4 0
-STABLE 2 2
-TREASURE 0 0
-->
-25
-
-The river
-5 5 4
-DRAGON
-IGGGI
-GGGGG
-WWWWW
-GGGGG
-IIGGG
-HOUSE 0 4
-CASTLE 0 0
-BLACKSMITH 1 4
-DRAGON 4 0
-->
-17
-
-Rise of the chaos wizard
-5 5 4
-TREASURE
-GGGGI
-IGGGG
-RRRRR
-IGGGG
-GGGGI
-HOUSE 4 4
-CASTLE 4 0
-WIZARD 0 3
-TREASURE 0 1
--> 18
-*/
+export const theRiver = [
+    "5 5 4",
+    "DRAGON",
+    "IGGGI",
+    "GGGGG",
+    "WWWWW",
+    "GGGGG",
+    "IIGGG",
+    "HOUSE 0 4",
+    "CASTLE 0 0",
+    "BLACKSMITH 1 4",
+    "DRAGON 4 0",
+];
+export const riseOfTheChaosWizard = [
+    "5 5 4",
+    "TREASURE",
+    "GGGGI",
+    "IGGGG",
+    "RRRRR",
+    "IGGGG",
+    "GGGGI",
+    "HOUSE 4 4",
+    "CASTLE 4 0",
+    "WIZARD 0 3",
+    "TREASURE 0 1",
+];
--- src/test/testcases.spec.ts
+++ src/test/testcases.spec.ts
@@ -1,12 +1,34 @@
-import { runToTheHills } from "../data/TestProblems";
+import { hallOfTheMountainKing, riseOfTheChaosWizard, runToTheHills, theRiver } from "../data/TestProblems";
 import { Solver } from "../logic/Solver";
 import { Problem } from "../model/Problem";
 import { Solution } from "../model/Solution";
 
+const testForDuration = (encodedProblem: string[], numberOfTurns: number) => {
+    const problem: Problem = new Problem(encodedProblem);
+    const solution: Solution = Solver.solve(problem);
+    expect(solution.numberOfTurns).toEqual(numberOfTurns);
+}
+
 describe("Run to the Hills", () =>
     test("should resolve in 9 turns", () => {
-        const problem: Problem = new Problem(runToTheHills);
-        const solution: Solution = Solver.solve(problem);
-        expect(solution.numberOfTurns).toEqual(9);
+        testForDuration(runToTheHills, 9);
     })
 );
+
+describe("Hall of the mountain king", () => {
+    test("should resolve in 25 turns", () => {
+        testForDuration(hallOfTheMountainKing, 25);
+    });
+});
+
+describe("The River", () => {
+    test("should resolve in 17 turns", () => {
+        testForDuration(theRiver, 17);
+    });
+});
+
+describe("Rise of the chaos wizard", () => {
+    test("should resolve in 18 turns", () => {
+        testForDuration(riseOfTheChaosWizard, 18);
+    });
+});
